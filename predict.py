89d5d8e3c671c3e7014ec68720221b29020a5316
mohammedi <hm@big-mama.io>
2018-05-05 00:36:34 +0100

    add model parameter for predict.py script

--- predict.py
+++ predict.py
@@ -2,7 +2,8 @@ import numpy as np
 import json
 from keras.models import load_model
 import sys
-i=np.array([json.loads(sys.argv[1])])
-model=load_model('model.h5')
-o=model.predict(i)
+
+i = np.array([json.loads(sys.argv[2])])
+model = load_model(sys.argv[1])
+o = model.predict(i)
 print(o)
